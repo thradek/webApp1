7a39a8054f477b42a6ad787f9e2aa8df8582e264
Thomas Hradek <thradek@ovistechnologies.com>
2019-04-01 12:59:40 +0000

    Added SNS to lambda function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -6,18 +6,27 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')#, config=Config(signature_version='s3v4'))
-    build_bucket = s3.Bucket('portfoliobuild.thomashradek.com')
-    portfolio_bucket = s3.Bucket('portfolio.thomashradek.com')
-    portfolio_zip = StringIO.StringIO()
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:481887095455:PortfolioDeployTopic')
+    try:
+        s3 = boto3.resource('s3')#, config=Config(signature_version='s3v4'))
+        build_bucket = s3.Bucket('portfoliobuild.thomashradek.com')
+        portfolio_bucket = s3.Bucket('portfolio.thomashradek.com')
+        portfolio_zip = StringIO.StringIO()
+        
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+                
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio Deployed successfully")
+    except:
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio NOT Deployed successfully")
+        raise
     
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-    
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
